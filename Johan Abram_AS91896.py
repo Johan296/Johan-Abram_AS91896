d061e08683d50b5fc274779b3409ae024db66fc0
Johan296 <159972047+Johan296@users.noreply.github.com>
2025-06-15 13:25:37 +1200

    lotta work

--- Johan Abram_AS91896.py
+++ Johan Abram_AS91896.py
@@ -1,64 +1,95 @@
-#This is a simple Pokémon game where the player chooses a Pokémon and battles against another Pokémon.
+# This is a simple Pokémon game where the player chooses a Pokémon and battles against another Pokémon.
 
-print("""
-______     _                                ______                       
-| ___ \   | |                              |  __ \                      
-| |_/ /__ | | _____ _ __ ___   ___  _ __   | |  \/ __ _ _ __ ___   ___  
-|  __/ _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \  | | __ / _` | '_ ` _ \ / _ \ 
-| | | (_) |   <  __/ | | | | | (_) | | | | | |_\ \ (_| | | | | | |  __/ 
-\_|  \___/|_|\_\___|_| |_| |_|\___/|_| |_|  \____/\__,_|_| |_| |_|\___| 
+print(""" 
+  ____       _        _                         
+ |  _ \ ___ | | _____| |_ _ __ ___   ___  _ __  
+ | |_) / _ \| |/ / _ \ __| '_ ` _ \ / _ \| '_ \ 
+ |  __/ (_) |   <  __/ |_| | | | | | (_) | | | |
+ |_|   \___/|_|\_\___|\__|_| |_| |_|\___/|_| |_|                                             
 """)
 
-print("Welcome to the Pokémon Game!")
-print("In this game, you will choose a Pokémon and battle against another Pokémon.")
-game = input("Are you ready to start your Pokémon adventure? (yes/no): ").lower()
-if game == "yes":
-    print("Lets get started!")
-elif game == "no":
-    print("No problem! You can come back anytime to play.")
-    exit()
+print("Welcome to your Pokétmon Adventure!")
 trainer_name = input("First of all, Please enter your trainer name: ")
-print("Welcome, {}! Let's start your Pokémon adventure!".format(trainer_name))
+print(f"\nWelcome, trainer {trainer_name}! Let's start your Pokétmon adventure!")
+print("You wake up in Paimon Town, where you have always dreamed of becoming a Pokétmon Trainer.")
+print("You get a call from Professor Clark, who tells you to come to his lab.")
 
-print("------------------------------------")
+choice = input("Do you go to the lab? (yes/no): ").lower()
+if choice == "yes":
+        print("\nYou head to Professor Clark's lab with excitement!.")
+elif choice == "no":
+        print("You decide to stay home and play video games instead. Maybe another time!")
+        exit()
 
-print("Here is the list of available starter Pokémons and their type charts:")
-print("This is the Type Chart,\n  Fire > Grass\n  Water > Fire\n  Electric > Water\n  Psychic > Electric\n  Ghost > Psychic")
-print("------------------------------------")
-print("Choose your Pokémon wisely, as each type has its strengths and weaknesses!")
-print("a)Pikachu: Type: Electric, hp: 190, attack:20"),
-print("b)Squitle: Type: Water, hp: 220, attack:20"),
-print("c)Charmander: Type: Fire, hp: 200, attack:20"),
-print("d)Bulbasaur: Type: Grass, hp: 230, attack:20")
-print("e)Kadabra: Type: Psychic, hp: 215, attack:22")
-print("f)Ghastly: Type: Dark, hp: 205, attack:20")
+print("You arrive at Professor Clark's lab, where you see him waiting for you along with his nephew Barley, who happens to be your rival.")
+print("Professor greets you and you greet him back and then he tells you that Barley has already chosen his first Pokétmon which is a Flametail,"
+            " the one you wanted to choose. Barley knows that you wanted it so he smirks and says, "
+            f"'Too bad {trainer_name}, first come first serve!'")
+print("You feel a bit sad, but Professor Clark assures you that there are other great Pokétmons available for you to choose from.")
 
-# The player's choice of Pokémon
-choose_pokemon = input("Please choose your Pokémon (Enter: a,b,c,d,e,f): ").lower()
-while choose_pokemon not in ["a", "b", "c", "d", "e", "f"]:
-    print("Invalid choice! Please choose a valid Pokémon.")
-    choose_pokemon = input("Please choose your Pokémon (Enter: a,b,c,d,e,f): ").lower()
-if choose_pokemon == "a":
-    print("That's a great choice, Pikachu is very friendly!")
-    player_pokemon = {"name": "Pikachu", "type": "Electric", "hp": 190, "attack": 20}
-elif choose_pokemon == "b":
-    print("You have chosen Squirtle!")
-    player_pokemon = {"name": "Squirtle", "type": "Water", "hp": 220, "attack": 20}
-elif choose_pokemon == "c":
-    print("You have chosen Charmander!")
-    player_pokemon = {"name": "Charmander", "type": "Fire", "hp": 200, "attack": 20}
-elif choose_pokemon == "d":
-    print("You have chosen Bulbasaur!")
-    player_pokemon = {"name": "Bulbasaur", "type": "Grass", "hp": 230, "attack": 20}
-elif choose_pokemon == "e":
-    print("Unique choice, no one chooses this usually!")
-    player_pokemon = {"name": "Kadabra", "type": "Psychic", "hp": 215, "attack": 22}
-elif choose_pokemon == "f":
-    print("Hmm Ghastly, scary choice!")
-    player_pokemon = {"name": "Ghastly", "type": "Dark", "hp": 205, "attack": 20}
+choose = input("\nHe asks if you are you ready to choose your first Pokétmon? (yes/no): ").lower()
+if choose == "yes":
+        print("Professor Clark shows you the remaining Pokétmons available for you to choose from.\n")
+elif choose == "no":
+        print("You decide to leave the lab and go home. Maybe you'll come back later.")
+        exit()
 
-print("------------------------------------")
-print("{} has chosen {} which is a {} type and has {} hp.".format( trainer_name, player_pokemon["name"], player_pokemon["type"], player_pokemon["hp"]))
-print("Now, the story begins...")
-print("------------------------------------")
+print("Here are the available Pokétmons, choose them wisely, as each type has its strengths and weaknesses!")
+print("a) Nimbolt:   Type: Electric, hp: 190, attack: 20 | Strong against: Water   | Weak against: Psychic")
+print("b) Hydrozoa:  Type: Water,    hp: 220, attack: 20 | Strong against: Fire    | Weak against: Electric")
+print("c) Ivyrex:    Type: Grass,    hp: 230, attack: 20 | Strong against: Water   | Weak against: Fire")
+print("d) Hollowby:  Type: Dark,     hp: 205, attack: 20 | Strong against: Psychic | Weak against: Electric")
+
+# The player's choice of Pokétmon
+choose_poketmon = input("\nPlease choose your Pokétmon (Enter: a,b,c,d): ").lower()
+while choose_poketmon not in ["a", "b", "c", "d"]:
+        print("Invalid choice! Please choose a valid Pokétmon.")
+        choose_poketmon = input("Please choose your Pokétmon (Enter: a,b,c,d): ").lower()
+
+if choose_poketmon == "a":
+        print("That's a great choice, Nimbolts are very friendly!")
+        player_poketmon = {"name": "Nimbolt", "type": "Electric", "hp": 190, "attack": 20}
+elif choose_poketmon == "b":
+        print("Solid choice! Hydrozoa is always cool under pressure!")
+        player_poketmon = {"name": "Hydrozoa", "type": "Water", "hp": 220, "attack": 20}
+elif choose_poketmon == "c":
+        print("Strong choice!")
+        player_poketmon = {"name": "Ivyrex", "type": "Grass", "hp": 230, "attack": 20}
+elif choose_poketmon == "d":
+        print("Hmm Hollowby, scary choice!")
+        player_poketmon = {"name": "Hollowby", "type": "Dark", "hp": 205, "attack": 20}
+print("")
+
+print(f"Trainer {trainer_name} has chosen {player_poketmon['name']} which is a {player_poketmon['type']} type and has {player_poketmon['hp']} hp.")
+print("You are now ready to start your adventure with your new Pokétmon!")
+print("")
+print("Professor Clark gives you the Pokétmon Ball and tells you to take good care of your new Pokétmon.")
+print("You thank him and start packing your bag with some essentials and wish farewell to Professor Clark and Barley.")
+# The player will now embark on his journey to catch more Pokémon
+
+
+# Opponent Pokémon
+opponent = {"name": "Pigot", "type": "Flying", "hp": 150, "attack": 15}
+print("A wild {} appeared! It has {} hp.".format(opponent["name"], opponent["hp"]))
+
+# Begin battle loop
+while player_poketmon["hp"] > 0 and opponent["hp"] > 0:
+    print("What do you want to do? (attack/run)")
+    action = input("> ").lower()
     
+    if action == "attack":
+        opponent["hp"] -= player_poketmon["attack"]
+        print(f"You attacked {opponent['name']}! Its HP is now {opponent['hp']}.")
+        if opponent["hp"] <= 0:
+            print("You won the battle!🎉")
+            break
+        player_poketmon["hp"] -= opponent["attack"]
+        print(f"{opponent['name']} attacked back! Your HP is now {player_poketmon['hp']}.")
+        if player_poketmon["hp"] <= 0:
+            print("You fainted... better luck next time! 💫")
+            break
+    elif action == "run":
+        print("You ran away safely.")
+        break
+    else:
+        print("Please choose a valid action.")
